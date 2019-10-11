f08f2253fb977e6629039d81e4b3f206849f08bb
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-11 16:26:53 +0900

    Implementation of Delete Function of Category with Javascript Modal

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -19,12 +19,57 @@
                 {{ $category->name }}
               </td>
               <td>
-                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
+                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">
+                  Edit
+                </a>
+                <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $category->id }})">Delete</button>
               </td>
             </tr>
           @endforeach
         </tbody>
       </table>
+
+<!----------------------- Modal ------------------------------------------------------------------------------------------------>
+
+      <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <form action="" method="POST" id="deleteCategoryForm">
+            @csrf
+            @method('DELETE')
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Delete Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p class="text-center text-bold">
+                  Are you sure you want to delete this category?
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">No, Go Back</button>
+                <button type="submit" class="btn btn-danger">Yes, delete</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+
     </div>
   </div>
+@endsection
+
+<!------------------------ JavaScript --------------------------->
+
+@section('scripts')
+  <script>
+    function handleDelete(id){
+      let form = document.getElementById('deleteCategoryForm')
+      form.action = '/categories/' + id
+      $('#deleteModal').modal('show');
+    }
+  </script>
 @endsection
