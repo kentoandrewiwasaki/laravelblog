c4b8839adb278f8c038a7bdbf7a3677db760407b
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-17 22:54:23 +0900

    Update of PostsTableSeeder Changing Image

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -77,7 +77,7 @@ class PostsTableSeeder extends Seeder
             'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'content' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'category_id' => $category1->id,
-            'image' => 'posts/2.jpg',
+            'image' => 'posts/5.jpg',
             'published_at' => Carbon::create('2019', '10', '16')
         ]);
 
